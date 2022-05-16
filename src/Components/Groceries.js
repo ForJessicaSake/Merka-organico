9ce6e5a8cbe06d1eebb1efb64094b008138f567b
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-05-16 02:27:39 +0100

    I made some changes to the service box

--- src/Components/Groceries.js
+++ src/Components/Groceries.js
@@ -1,23 +1,23 @@
-import React from "react";
+// import React from "react";
 
 
-const Groceries= ({ item }) => {
-    const {  title, price, img } = item;
-    return (
-        <main className="groceries-wrapper">
-            <section className="groceries">
-                <div className="image-box">
-                    <img src={img} alt='case-images'></img>
-                </div>
-                <article className="details">
-                    <p>{title}</p>
-                    <p className="price">{price}</p>
-                    <button>Add to Cart</button>
-                </article>
-            </section>
-        </main>
+// const Groceries= ({ item }) => {
+//     const {  title, price, img } = item;
+//     return (
+//         <main className="groceries-wrapper">
+//             <section className="groceries">
+//                 <div className="image-box">
+//                     <img src={img} alt='case-images'></img>
+//                 </div>
+//                 <article className="details">
+//                     <p>{title}</p>
+//                     <p className="price">{price}</p>
+//                     <button>Add to Cart</button>
+//                 </article>
+//             </section>
+//         </main>
 
-    )
-}
+//     )
+// }
 
-export default Groceries;
+// export default Groceries;
--- src/Components/Sales.js
+++ src/Components/Sales.js
@@ -1,17 +1,107 @@
 import React from 'react';
-import Groceries from './Groceries';
-import List from './data';
+import { useState } from 'react';
+// import Groceries from './Groceries';
 import './Sales.css';
 
 
 function Sales() {
+    const [Lists, setLists] = useState([
+        {
+            id: 1,
+            title: "Strawberry",
+            price: "$25",
+            img: require("../Images/strawberry.png"),
+        }, {
+            id: 2,
+            title: "Banana",
+            price: "$30",
+            img: require("../Images/banana.png"),
+        },
+
+        {
+            id: 3,
+            title: "Coconut",
+            price: "$40",
+            img: require("../Images/coconut.png"),
+        }, {
+            id: 4,
+            title: "Orange",
+            price: "$10",
+            img: require("../Images/orange.png"),
+        }, {
+            id: 5,
+            title: "Lime",
+            price: "$5",
+            img: require("../Images/lime.png"),
+        }, {
+            id: 6,
+            title: "Pineapple",
+            price: "$60",
+            img: require("../Images/pineapple.png"),
+        }, {
+            id: 7,
+            title: "Carrot",
+            price: "$15",
+            img: require("../Images/carrot.png"),
+        }, {
+            id: 8,
+            title: "Onion",
+            price: "$10",
+            img: require("../Images/onion.png"),
+        }, {
+            id: 9,
+            title: "Avocado",
+            price: "$25",
+            img: require("../Images/avocado.png"),
+        }, {
+            id: 10,
+            title: "Tomato",
+            price: "$15",
+            img: require("../Images/tomato.png"),
+        }, {
+            id: 11,
+            title: "Egg",
+            price: "$5",
+            img: require("../Images/egg.png"),
+        }, {
+            id: 12,
+            title: "Green Pepper",
+            price: "$10",
+            img: require("../Images/greenpepper.png"),
+        }, {
+            id: 10,
+            title: "Apple",
+            price: "$70",
+            img: require("../Images/apple.png"),
+        }, {
+            id: 11,
+            title: "Grape",
+            price: "$100",
+            img: require("../Images/grape.png"),
+        }, {
+            id: 12,
+            title: "Watermelon",
+            price: "$20",
+            img: require("../Images/watermelon.png"),
+        },
+    ]);
+
     return (
         <section className="sales-wrapper" id='Sale'>
             <article className='sales-subtitle'>Inventory</article>
 
             <main className="sales-items">
-                {List.map((item) => (
-                    <Groceries key={item.id} item={item} />
+                {Lists.map((List) => (
+                    <section className='groceries' key={List.id}>
+                        <div className="image-box">
+                            <img src={List.img} alt='case-images'></img>
+                            <article className='details'>
+                                <p>{List.title}</p>
+                                <p className='price'>{List.price}</p>
+                                <button>Add to Cart</button>
+                            </article>
+                        </div>
+                    </section>
                 ))}
             </main>
         </section>
