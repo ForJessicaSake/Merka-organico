c3c3e26b8398d4009291ba269486a797ee3001b3
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-05-20 16:56:00 +0100

    i worked on the services img

--- src/Components/Sales.js
+++ src/Components/Sales.js
@@ -6,25 +6,19 @@ import './Sales.css';
 
 function Sales() {
     const [Lists, setLists] = useState(null);
-    const [isLoading, setisLoading] = useState(true);
-
  
 useEffect(()=>{
-    setTimeout(()=> {
-    fetch('http://localhost:5000/List')
+    fetch("http://localhost:5000/List")
     .then(res=>{
         return res.json();
     })
     .then(data=>{
         setLists(data);
-        setisLoading(false);
     });
-}, 1500);
 }, []);
     return (
         <section className="sales-wrapper" id='Sale'>
             <article className='sales-subtitle'>Inventory</article>
-            {isLoading && <button className='timeout-info'>Loading.....</button>}
 
             <main className="sales-items">
                 { Lists && Lists.map((List) => (
@@ -34,7 +28,7 @@ useEffect(()=>{
                             <article className='details'>
                                 <p>{List.title}</p>
                                 <p className='price'>{List.price}</p>
-                                <button className='salesbtn'>Add to Cart</button>
+                                <button>Add to Cart</button>
                             </article>
                         </div>
                     </section>
