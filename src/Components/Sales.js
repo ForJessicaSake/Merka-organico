bb75e72d23c779fa19e38a92cc7c6aabdacd33a1
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-05-21 19:10:54 +0100

    I Changes The Inventory Price

--- src/Components/Sales.js
+++ src/Components/Sales.js
@@ -8,92 +8,92 @@ function Sales() {
         {
             id: 1,
             title: "Strawberry",
-            price: "$25",
+            price: "$9",
             img: "../Images/strawberry.png"
         },
         {
             id: 2,
             title: "Banana",
-            price: "$30",
+            price: "$5",
             img: "../Images/banana.png"
         },
 
         {
             id: 3,
             title: "Coconut",
-            price: "$40",
+            price: "$7",
             img: "../Images/coconut.png"
         },
         {
             id: 4,
             title: "Orange",
-            price: "$10 ",
+            price: "$3",
             img: "../Images/orange.png"
         },
         {
             id: 5,
             title: "Lime",
-            price: "$5 ",
+            price: "$4",
             img: "../Images/lime.png"
         },
         {
             id: 6,
             title: "Pineapple",
-            price: "$60",
+            price: "$10",
             img: "../Images/pineapple.png"
         },
         {
             id: 7,
             title: "Carrot",
-            price: "$15",
+            price: "$6",
             img: "../Images/carrot.png"
         },
         {
             id: 8,
             title: "Onion",
-            price: "$10",
+            price: "$2",
             img: "../Images/onion.png"
         },
         {
             id: 9,
             title: "Avocado",
-            price: "$25 ",
+            price: "$10 ",
             img: "../Images/avocado.png"
         },
         {
             id: 10,
             title: "Tomato",
-            price: "$15",
+            price: "$3",
             img: "../Images/tomato.png"
         },
         {
             id: 11,
             title: "Egg",
-            price: "$5",
+            price: "$1",
             img: "../Images/egg.png"
         },
         {
             id: 12,
             title: "Green Pepper",
-            price: "$10",
+            price: "$2",
             img: "../Images/greenpepper.png"
         },
         {
             id: 13,
             title: "Apple",
-            price: "$70",
+            price: "$10",
             img: "../Images/apple.png"
         },
         {
             id: 14,
             title: "Grape",
-            price: "$100",
+            price: "$10",
             img: "../Images/grape.png"
         },
         {
             id: 15,
             title: "Watermelon",
-            price: "$20",
+            price: "$5",
             img: "../Images/watermelon.png"
         }
     ]);
